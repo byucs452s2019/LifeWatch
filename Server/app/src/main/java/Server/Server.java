f32851efa5602d4a50e454e977f71d7a4ff5c833
haoyucn <haoyu_cn@hotmail.com>
2019-05-23 14:30:45 -0600

    added

--- Server/app/src/main/java/Server/Server.java
+++ Server/app/src/main/java/Server/Server.java
@@ -1,4 +1,6 @@
 package Server;
+import java.util.Calendar;
+
 import Database.*;
 import Database.Model.UserModel;
 
@@ -11,23 +13,18 @@ public class Server {
     public static void main(String[] argvs) {
         SQLDBConnection conn =  new SQLDBConnection();
         conn.createDBFile();
-        UserModel user = new UserModel("hao", "qqqqqq", "@@@", 26, "weeeb");
+
+        // populatiing the DAO
         UserDAO userDAO = new UserDAO();
+        UserModel user = new UserModel("J", "password", "@@@", 16, "weeb");
+        userDAO.insert(user);
+        user = new UserModel("T", "password", "@@@", 18, "weeb");
+        userDAO.insert(user);
+        user = new UserModel("M", "password", "@@@", 20, "weeb");
+        userDAO.insert(user);
+        user = new UserModel("R", "password", "@@@", 22, "weeb");
+        userDAO.insert(user);
 
-//        if (userDAO.insert(user)){
-//            System.out.println("successfully added user");
-//        }
-//        else {
-//            System.out.println("failed to add user");
-//        }
-        userDAO.update();
-
-        UserModel cuser = userDAO.getUserByUserName("hao");
-        if (cuser == null) {
-            System.out.println("failed to get user");
-        }
-        else {
-            System.out.println(cuser.toString());
-        }
+        MotionDAO motionDAO = new MotionDAO();
     }
 }
