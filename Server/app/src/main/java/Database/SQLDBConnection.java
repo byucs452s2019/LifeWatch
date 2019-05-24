ea2b9d69379e1b60f1d867fb801aeffefbf4f56c
haoyucn <haoyu_cn@hotmail.com>
2019-05-24 12:38:02 -0600

    added a screenshot

--- Server/app/src/main/java/Database/SQLDBConnection.java
+++ Server/app/src/main/java/Database/SQLDBConnection.java
@@ -6,7 +6,7 @@ import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
- import java.sql.ResultSet;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
